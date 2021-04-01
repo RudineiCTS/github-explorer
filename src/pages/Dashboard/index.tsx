cb7e6d70e7b6ba3d809cf5c6dda97464edfabd31
Rudinei Costa <costaaugustodev@gmail.com>
2021-04-01 17:29:05 -0300

    wip: building dashbord

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
-import {Title} from './styled'
+import {Title, Form, Repository} from './styled'
+import {FiChevronRight} from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
 
 const Dashboard:React.FC =()=> {
   return(
-    <Title>Dashboard</Title>
+    <>
+      <img src={logo} alt="Github explorer"/>
+      <Title>Explore repositórios no Github</Title>
+
+
+      <Form>
+        <input type="text" placeholder="digite aqui"/>
+        <button>Pesquisar</button>
+      </Form>
+
+      <Repository>
+        <a href="#">
+          <img src="https://avatars.githubusercontent.com/u/47544016?v=4" alt="image Dev"/>
+          <div>
+          <strong>GitHub-explorer\bootcamp</strong>
+            <p>Projeto desenvolvido para treinamento em react</p>
+          </div>
+
+          <FiChevronRight/>
+        </a>
+
+        <a href="#">
+          <img src="https://avatars.githubusercontent.com/u/47544016?v=4" alt="image Dev"/>
+          <div>
+          <strong>GitHub-explorer\bootcamp</strong>
+            <p>Projeto desenvolvido para treinamento em react</p>
+          </div>
+
+          <FiChevronRight/>
+        </a>
+      </Repository>
+    </>
   )
 }
 export default  Dashboard;
