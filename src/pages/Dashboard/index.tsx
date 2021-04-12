d9804331ab4f0a41c4528ab871ab18b309c2e21d
Rudinei Costa <costaaugustodev@gmail.com>
2021-04-12 03:30:19 -0300

    feat: connect to api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,41 +1,69 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import {Title, Form, Repository} from './styled'
 import {FiChevronRight} from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
+interface Repositories{
+  full_name: string;
+  description:string;
+  owner:{
+    login: string;
+    avatar_url:string;
+  }
+}
+
+
 const Dashboard:React.FC =()=> {
+  const [newRepo, setNewRepo]= useState('');
+  const [repositories, setRespositories] = useState<Repositories[]>([]);
+
+  async function handleAddRepository
+  (event: FormEvent<HTMLFormElement>): Promise<void>{
+
+    event.preventDefault();
+
+    const response = await api.get<Repositories>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRespositories([...repositories, repository]);
+    console.log(repositories);
+  }
   return(
     <>
       <img src={logo} alt="Github explorer"/>
       <Title>Explore repositórios no Github</Title>
 
 
-      <Form>
-        <input type="text" placeholder="digite aqui"/>
+      <Form
+        onSubmit={handleAddRepository}
+      >
+        <input
+        type="text"
+        value={newRepo}
+        onChange={(e) => setNewRepo(e.target.value)}
+        placeholder="digite aqui"/>
         <button>Pesquisar</button>
       </Form>
 
       <Repository>
-        <a href="#">
-          <img src="https://avatars.githubusercontent.com/u/47544016?v=4" alt="image Dev"/>
-          <div>
-          <strong>GitHub-explorer\bootcamp</strong>
-            <p>Projeto desenvolvido para treinamento em react</p>
-          </div>
+        {repositories.map(repository => (
+            <a
+            key={repository.full_name}
+            href="#">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+            <div>
+            <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight/>
-        </a>
+            <FiChevronRight/>
+          </a>
 
-        <a href="#">
-          <img src="https://avatars.githubusercontent.com/u/47544016?v=4" alt="image Dev"/>
-          <div>
-          <strong>GitHub-explorer\bootcamp</strong>
-            <p>Projeto desenvolvido para treinamento em react</p>
-          </div>
-
-          <FiChevronRight/>
-        </a>
+          )
+        )}
       </Repository>
     </>
   )
