51aadebf686f24fc18ad106882ae30def616a8a1
Rudinei Costa <costaaugustodev@gmail.com>
2021-05-17 15:45:05 -0300

    feat: styled repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import {Link, useRouteMatch} from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+
+import {Header, RepositoryInfo, Issues} from './styles'
+
 
 type RepositoryParams ={
   repository: string,
@@ -10,7 +15,50 @@ const Repository:React.FC =()=> {
   const {params}= useRouteMatch<RepositoryParams>();
   // console.log(params)
   return(
-    <h1>Repository : {params.repository} </h1>
+    <>
+      <Header>
+        <img src={logo}/>
+        <Link to='/'>
+          <FiChevronLeft
+            size={16}
+          />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://cdn.falauniversidades.com.br/wp-content/uploads/2020/10/13111828/One-Piece-dublado-na-Netflix.jpg" alt=""/>
+          <div>
+            <strong>nome do repo</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>47</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link
+            to= {'#'}>
+            <div>
+            <strong>Nome do repositório</strong>
+              <p>Descrição</p>
+            </div>
+            <FiChevronRight/>
+          </Link>
+      </Issues>
+   </>
   )
 }
 export default  Repository;
